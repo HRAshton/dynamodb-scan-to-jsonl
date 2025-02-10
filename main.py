161ec6bef105004cb43109d3b5fa3006b4a1dc03
HRAshton <12210721+HRAshton@users.noreply.github.com>
2025-02-10 02:41:29 +0100

    Fix style issues

--- main.py
+++ main.py
@@ -1,3 +1,17 @@
+"""
+Scans all tables in the DynamoDB and saves the output to the .jsonl files.
+
+It reads the configuration from the environment variables:
+- JSONLS_DIR: The directory where the .jsonl files are stored.
+- SORT_KEYS: Whether to sort the keys in the JSON lines.
+- SORTABLE_PRIMARY_KEY: Name of the primary key to sort the items by.
+
+If run with the default configuration, it will:
+1. Find all .jsonl files in the parent directory of this repository.
+2. Calculate required table names from the file names.
+3. Scan all tables and save the output to the corresponding files.
+"""
+
 import json
 import logging
 import os
@@ -10,24 +24,26 @@ logger = logging.getLogger()
 
 # The directory where the .jsonl files are stored.
 # Default: parent directory of this repository.
-JSONLS_DIR = os.environ.get("JSONLS_DIR", os.path.join(os.path.realpath(__file__), os.pardir, os.pardir))
+JSONLS_DIR = os.environ.get('JSONLS_DIR', os.path.join(os.path.realpath(__file__),
+                                                       os.pardir,
+                                                       os.pardir))
 
 # Whether to sort the keys in the JSON lines.
 # Default: true
-SORT_KEYS = os.environ.get("SORT_KEYS", "true").lower() == "true"
+SORT_KEYS = os.environ.get('SORT_KEYS', 'true').lower() == 'true'
 
 # Name of the primary key to sort the items by.
 # If not specified, the items will not be sorted.
 # Default: None
-SORTABLE_PRIMARY_KEY = os.environ.get("SORTABLE_PRIMARY_KEY", 'userId')
+SORTABLE_PRIMARY_KEY = os.environ.get('SORTABLE_PRIMARY_KEY', 'userId')
 
 
 def get_tables_with_files() -> dict[str, str]:
     """Returns all .jsonl files from the parent directory"""
     return {
-        name.replace(".jsonl", ""): os.path.abspath(os.path.join(JSONLS_DIR, name))
+        name.replace('.jsonl', ''): os.path.abspath(os.path.join(JSONLS_DIR, name))
         for name in os.listdir(JSONLS_DIR)
-        if name.endswith(".jsonl")
+        if name.endswith('.jsonl')
     }
 
 
@@ -38,9 +54,9 @@ def scan_table(client, table_name: str) -> list[dict[str, Any]]:
     items = []
     for page in response_iterator:
         items.extend(page['Items'])
-        logger.info(f"Scanned {len(items)} items from table {table_name}")
+        logger.info('Scanned %d items from table %s', len(items), table_name)
 
-    logger.info(f"Table {table_name} scanned successfully, {len(items)} items")
+    logger.info('Table %s scanned successfully, %d items', table_name, len(items))
     return items
 
 
@@ -50,23 +66,24 @@ def save_items_to_file(items: list[dict], output_file: str) -> None:
         # Each field is a dictionary, so we need to stringify it.
         items.sort(key=lambda x: str(x.get(SORTABLE_PRIMARY_KEY)))
 
-    with open(output_file, "w", encoding="utf-8") as file:
+    with open(output_file, 'w', encoding='utf-8') as file:
         for item in items:
-            file.write(json.dumps(item, sort_keys=SORT_KEYS) + "\n")
+            file.write(json.dumps(item, sort_keys=SORT_KEYS) + '\n')
 
 
 def main():
+    """Scans all tables and saves the output to the files"""
     client = boto3.Session().client('dynamodb')
     tables_with_files = get_tables_with_files()
     for input_table, output_file in tables_with_files.items():
-        logger.info(f"Scanning table {input_table} to {output_file}")
+        logger.info('Scanning table %s and saving to file %s', input_table, output_file)
         items = scan_table(client, input_table)
         save_items_to_file(items, output_file)
 
-    logger.info("All tables scanned successfully")
+    logger.info('All tables scanned successfully')
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
 
     logger.info(json.dumps({
@@ -76,11 +93,11 @@ if __name__ == "__main__":
     }, indent=2))
 
     if len(sys.argv) > 1:
-        logger.info(f"Using AWS profile: {sys.argv[1]}")
-        os.environ["AWS_PROFILE"] = sys.argv[1]
-    elif "AWS_PROFILE" in os.environ:
-        logger.info(f"Using AWS profile: {os.environ['AWS_PROFILE']}")
+        logger.info('Using AWS profile: %s', sys.argv[1])
+        os.environ['AWS_PROFILE'] = sys.argv[1]
+    elif 'AWS_PROFILE' in os.environ:
+        logger.info('Using AWS profile: %s', os.environ['AWS_PROFILE'])
     else:
-        logger.info("Using default AWS profile")
+        logger.info('Using default AWS profile')
 
     main()
